932545d664a64dee250a338094ce8b3d16a4dd63
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-05-25 16:32:09 +0200

    Add validate tests

--- tests/test_metayaml.py
+++ tests/test_metayaml.py
@@ -1,7 +1,21 @@
 import unittest
-import metayaml
+from click.testing import CliRunner
+
+from metayaml.cli import *
 
 class TestMetayaml(unittest.TestCase):
     def test_trivial(x):
         assert 1 + 1 == 2
 
+    def test_parse_string(x):
+        assert parse_string("foo") == "foo"
+        assert parse_string("True") == True
+        assert parse_string("1.1") == 1.1
+    
+    def test_validate(x):
+        runner = CliRunner()
+        result = runner.invoke(validate, ["example/EU/meta.yml", "example/schema.yml"])
+        assert result.exit_code == 0
+
+        result = runner.invoke(validate, ["example/EU/de.txt.meta.yml", "example/schema.yml"])
+        assert result.exit_code == 1
